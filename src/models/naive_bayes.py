19eb513498aefe463de8b07ba4fae7da579a219f
mrafie1 <rafie.muhammad2007@gmail.com>
2026-07-30 15:59:45 -0400

    edited skeleton code

--- src/models/naive_bayes.py
+++ src/models/naive_bayes.py
@@ -1,4 +1,6 @@
-from model import load_data, train_model, evaluate_model, predict_model, plot_history
+from scipy.stats import loguniform
+
+from model import load_data, train_model, evaluate_model
 from sklearn.naive_bayes import GaussianNB
 
 def build_model(
@@ -10,5 +12,35 @@ def build_model(
         priors=priors,
     )
 
+
 if __name__ == "__main__":
     print("I am running!")
+
+    X_train, y_train, X_test, y_test = load_data()
+
+    model = build_model()
+
+    hyperparams = {
+        "var_smoothing": loguniform(1e-12, 1e-7),
+    }
+
+    results = train_model(
+        model=model,
+        name="naive_bayes",
+        X_train=X_train,
+        y_train=y_train,
+        hyperparams=hyperparams,
+        n_iter=10,
+    )
+
+    metrics = evaluate_model(
+        results["model"],
+        X_test,
+        y_test,
+    )
+
+    print("Best parameters:", results["best_params"])
+    print("CV accuracy:", results["cv_accuracy"])
+    
+    print("Test accuracy:", metrics["accuracy"])
+    print("Confusion Matrix:", metrics["confusion_matrix"])
--- src/models/neural_network.py
+++ src/models/neural_network.py
@@ -1,4 +1,5 @@
 from sklearn.neural_network import MLPClassifier
+from scipy.stats import randint, loguniform
 from model import load_data, train_model, evaluate_model, predict_model, plot_history
 
 def build_model(
@@ -8,7 +9,7 @@ def build_model(
     learning_rate_init=0.001,
     alpha=0.0001,
     batch_size=32,
-    max_iter=200,
+    max_iter=500,
     early_stopping=False,
     random_state=42,
 ):
@@ -26,3 +27,40 @@ def build_model(
 
 if __name__ == "__main__":
     print("I am running!")
+    X_train, y_train, X_test, y_test = load_data()
+
+    model = build_model()
+
+    hyperparams = {
+    "hidden_layer_sizes": [
+        (50,),
+        (100,),
+        (100, 50),
+        (200, 100),
+    ],
+    "activation": ["relu", "tanh"],
+    "alpha": loguniform(1e-5, 1e-2),
+    "learning_rate_init": loguniform(1e-4, 1e-2),
+    "batch_size": randint(32, 256),
+    "early_stopping": [True],
+    }
+
+    results = train_model(
+        model=model,
+        name="neural_network",
+        X_train=X_train,
+        y_train=y_train,
+        hyperparams=hyperparams,
+    )
+
+    metrics = evaluate_model(results["model"], X_test, y_test)
+
+
+    print("Best parameters:", results["best_params"])
+    print("CV accuracy:", results["cv_accuracy"])
+
+    print("Test accuracy:", metrics["accuracy"])
+    print("Confusion Matrix:")
+    print(metrics["confusion_matrix"])
+
+    plot_history(results["model"])
